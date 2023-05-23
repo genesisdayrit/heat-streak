280cf1b5cb47d2d33c65250012122bf59b81f6d7
genesisdayrit <genesisdayrit@gmail.com>
2023-05-22 23:30:12 -0500

    horizontal heatmap

--- heatmap.py
+++ heatmap.py
@@ -4,14 +4,23 @@ import matplotlib.pyplot as plt
 # Generate random data for 365 days.
 data = np.random.randint(0, 11, (365,))
 
+# Calculate the number of missing values to make the data length divisible by 7.
+padding = (7 - len(data) % 7) % 7
+
+# Append the missing values to the data.
+data = np.append(data, np.zeros(padding))
+
 # Reshape data to have 7 columns, representing days of the week.
 reshaped_data = np.reshape(data, (-1, 7))
 
 # Create a figure and a set of subplots.
 fig, ax = plt.subplots()
 
-# Create the heatmap.
-cax = ax.matshow(reshaped_data, cmap='hot')
+# Create the heatmap, transposing the data to rotate the heatmap 90 degrees.
+cax = ax.matshow(reshaped_data.T, cmap='Blues')
+
+# Hide y-tick labels.
+ax.set_yticks([])
 
 # Create a colorbar for the heatmap. The colorbar will show how the colors correspond to values.
 fig.colorbar(cax)
